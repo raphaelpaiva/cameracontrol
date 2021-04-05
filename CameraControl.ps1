a9b4cf0b159c9a829e5e1788441e7b3429f6db94
raphaeldpaiva <raphaeldpaiva@gmail.com>
2021-04-05 14:37:39 -0300

    Adding Record, stop Commands

--- CameraControl.ps1
+++ CameraControl.ps1
@@ -1,26 +1,58 @@
-$devices = @(
- 'fcff35d8',   # Mi9T
-  '0051736433' # Z3
-)
-
 $ADBShell_VideoCommand = "shell am start -a android.media.action.VIDEO_CAPTURE"
 $ADBShell_HomeCommand  = "shell input keyevent KEYCODE_HOME"
+$ADBShell_BackCommand  = "shell input keyevent KEYCODE_BACK"
 $ADBShell_RecordCommand  = "shell input keyevent KEYCODE_VOLUME_UP"
+
+$devices = @(
+  @{ name = "Mi9T"
+     id = 'fcff35d8'
+     record_accept_command = $ADBShell_BackCommand 
+  },
+  @{ name = "Z3"
+     id = '0051736433'
+     record_accept_command = $ADBShell_HomeCommand 
+  }
+)
+
+function Monitor {
+  foreach ($device in $devices)
+  {
+    $id = $device.id
+    Start-Process -FilePath "scrcpy.exe" -ArgumentList "--serial $id -b 2M -m800 --stay-awake" -RedirectStandardOutput "scrcpy-$id-out.txt" -RedirectStandardError "scrcpy-$id-err.txt"
+  }
+}
 
 function RunADBCommand {
   param (
-    [String]$device,
-    [String]$command
+    [String]$Device,
+    [String]$Command
   )
-  Start-Process -FilePath "adb.exe" -ArgumentList "-s $device $command" -RedirectStandardOutput "adb-$device-out.txt" -RedirectStandardError "adb-$device-err.txt" -Wait
+  Start-Process -FilePath "adb.exe" -ArgumentList "-s $Device $command" -RedirectStandardOutput "adb-$Device-out.txt" -RedirectStandardError "adb-$Device-err.txt" -Wait
 }
 
-foreach($device in $devices)
-{
-  Start-Process -FilePath "scrcpy.exe" -ArgumentList "--serial $device -b 2M -m800 --stay-awake" -RedirectStandardOutput "scrcpy-$device-out.txt" -RedirectStandardError "scrcpy-$device-err.txt"
-  RunADBCommand($device, $ADBShell_HomeCommand)
-  RunADBCommand($device, $ADBShell_VideoCommand)
-  RunADBCommand($device, $ADBShell_RecordCommand)
+function Record {
+  foreach ($device in $devices)
+  {
+    RunADBCommand -Device $device.id -Command $ADBShell_HomeCommand
+    RunADBCommand -Device $device.id -Command $ADBShell_VideoCommand
+    RunADBCommand -Device $device.id -Command $ADBShell_RecordCommand
+  }
+}
+function Stop {
+  foreach ($device in $devices)
+  {
+    RunADBCommand -Device $device.id -Command $ADBShell_RecordCommand
+    RunADBCommand -Device $device.id -Command $device.record_accept_command
+  }
 }
 
+$COMMAND = $args[0]
 
+$COMMANDS = @{
+  Record = (Get-Item function:Record)
+  Monitor = (Get-Item function:Monitor)
+  Stop = (Get-Item function:Stop)
+}
+
+Write-Output "Running $COMMAND"
+& $COMMANDS[$COMMAND]
